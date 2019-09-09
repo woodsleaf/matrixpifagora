2fae90f74b951d2b5edc4052d900c3fb7ae1e70c
Pavel Kononov <woodsleaf@gmail.com>
2019-09-09 03:55:24 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 import sys
+import os
+from tkinter import *
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox as messagebox
+from tkinter import Menu
+import re
+from functools import reduce
 import argparse
 parser = argparse.ArgumentParser(description='Enter birthday')
 parser.add_argument(
-    '-D',
+    '-d',
     '--birthday',
     default='17-11-1991',
-    help='provide an birthday -D=ddmmyyyy (default: 17-11-1991 )'
+    help='provide an birthday -d=ddmmyyyy (default: 17-11-1991 )'
 )
 parser.add_argument(
-    '-N',
+    '-n',
     '--fullname',
     default='ФамилияИмяОтчество',
-    help='provide an fullname -N=ФамилияИмяОтчество (default: ФамилияИмяОтчество )'
+    help='provide an fullname -n=ФамилияИмяОтчество (default: ФамилияИмяОтчество )'
 )
 my_namespace = parser.parse_args()
 print(my_namespace)
-import re
-from functools import reduce
 
 fullname = my_namespace.fullname
 indata = my_namespace.birthday
@@ -62,6 +68,12 @@ def fwn(indata):
     return strsum(re.sub(r'[.,;-]', '', indata))
 #test: print(fwn(indata))
 
+#from input to text
+def clickedBtn():
+    indata = lbl_birthday_value.get()
+    print(lbl_birthday_value.get())
+    return True
+
 #Begin==================================================
 
 firstworknumber = fwn(indata)
@@ -98,12 +110,154 @@ all = ''
 if (int(year) >= 2000):
     #после 2000 включительно
     print(firstworknumber, secondworknumber, '19', threeworknumber, fourworknumber)
+    dopnum = firstworknumber, secondworknumber, '19', threeworknumber, fourworknumber
     all = ''.join(dmyarr) + str(firstworknumber) + str(secondworknumber) + '19' + str(threeworknumber) + str(fourworknumber)
 else:
     print(firstworknumber, secondworknumber, threeworknumber, fourworknumber)
+    dopnum = firstworknumber, secondworknumber, threeworknumber, fourworknumber
     all = ''.join(dmyarr) + str(firstworknumber) + str(secondworknumber) + str(threeworknumber) + str(fourworknumber)
 
 print(all)
 print('[{}][{}][{}]'.format(findto(1, all),findto(4, all),findto(7, all)) )
 print('[{}][{}][{}]'.format(findto(2, all),findto(5, all),findto(8, all)) )
 print('[{}][{}][{}]'.format(findto(3, all),findto(6, all),findto(9, all)) )
+
+#Рисуем GUI (4 колонки/12строк)
+#Дата рождения  /Значение               /Темперамент
+#Доп. числа     /Значение               /Значение
+#Число судьбы   /Значение
+
+#Характер(1)            /Здоровье(4)        /Удача          /Цель
+#значение               /значение           /значение       /значение
+
+#Энергия(2)             /Логика-интуиция(5) /Долг(8)        /Семья
+#значение               /значение           /значение        /значение
+
+#Интерес-Порядок(3)     /Труд(6)            /Память-Ум(9)   /Привычки(8)
+#значение               /значение           /значение       /значение
+
+#Быт(4)                 /Талант(7)
+#значение               /значение
+
+window = Tk()
+#Заголовок окна
+window.title("Добро пожаловать в приложение matrix")
+#window.geometry('683x768')
+#Меню
+menu = Menu(window)
+new_item = Menu(menu) #, tearoff=0
+new_item.add_command(label='Новый')
+new_item.add_separator()
+new_item.add_command(label='Открыть')
+new_item.add_separator()
+new_item.add_command(label='Изменить')
+menu.add_cascade(label='Файл', menu=new_item)
+window.config(menu=menu)
+
+#zero
+lbl_birthday = Label(window, text="Дата рождения", font=("Arial Bold", 18))
+lbl_birthday.grid(column=0, row=0)
+lbl_birthday_value = Entry(window, width=10, font=("Arial", 14))
+lbl_birthday_value.insert(INSERT, indata)
+lbl_birthday_value.focus()
+lbl_birthday_value.grid(column=1, row=0)
+btn = Button(window, command=clickedBtn, text="Вычислить", bg="white", fg="black")
+btn.grid(column=2, row=0)
+lbl_temperament = Label(window, text="Темперамент", font=("Arial", 18))
+lbl_temperament.grid(column=3, row=0)
+
+lbl_dopnumbers = Label(window, text="Доп числа", font=("Arial Bold", 18))
+lbl_dopnumbers.grid(column=0, row=1)
+lbl_dopnumbers_value = Label(window, text="1st, 2st, 3st, 4st", font=("Arial", 14))
+lbl_dopnumbers_value['text'] = dopnum
+lbl_dopnumbers_value.grid(column=1, row=1)
+lbl_temperament_value = Label(window, text="value", font=("Arial", 14))
+lbl_temperament_value.grid(column=3, row=1)
+
+lbl_desteny = Label(window, text="Число судьбы", font=("Arial Bold", 18))
+lbl_desteny.grid(column=0, row=2)
+
+#three
+lbl_character = Label(window, text="Характер(1)", font=("Arial Bold", 18))
+lbl_character.grid(column=0, row=3)
+lbl_zdorovie = Label(window, text="Здоровье(4)", font=("Arial Bold", 18))
+lbl_zdorovie.grid(column=1, row=3)
+lbl_udacha = Label(window, text="Удача", font=("Arial Bold", 18))
+lbl_udacha.grid(column=2, row=3)
+lbl_target = Label(window, text="Цель(2)", font=("Arial Bold", 18))
+lbl_target.grid(column=3, row=3)
+
+lbl_character_value = Label(window, text="value", font=("Arial", 14))
+lbl_character_value['text'] = findto(1, all)
+lbl_character_value.grid(column=0, row=4)
+lbl_zdorovie_value = Label(window, text="value", font=("Arial", 14))
+lbl_zdorovie_value['text'] = findto(4, all)
+lbl_zdorovie_value.grid(column=1, row=4)
+lbl_udacha_value = Label(window, text="value", font=("Arial", 14))
+#lbl_udacha_value['text'] =
+lbl_udacha_value.grid(column=2, row=4)
+lbl_target_value = Label(window, text="value", font=("Arial", 14))
+lbl_target_value['text'] = findto(2, all)
+lbl_target_value.grid(column=3, row=4)
+
+#five
+lbl_energy = Label(window, text="Энергия(2)", font=("Arial Bold", 18))
+lbl_energy.grid(column=0, row=5)
+lbl_logika = Label(window, text="Логика(5)", font=("Arial Bold", 18))
+lbl_logika.grid(column=1, row=5)
+lbl_dolg = Label(window, text="Долг(8)", font=("Arial Bold", 18))
+lbl_dolg.grid(column=2, row=5)
+lbl_family = Label(window, text="Семья(2)", font=("Arial Bold", 18))
+lbl_family.grid(column=3, row=5)
+
+lbl_energy_value = Label(window, text="value", font=("Arial", 14))
+lbl_energy_value['text'] = findto(2, all)
+lbl_energy_value.grid(column=0, row=6)
+lbl_logika_value = Label(window, text="value", font=("Arial", 14))
+lbl_logika_value['text'] = findto(5, all)
+lbl_logika_value.grid(column=1, row=6)
+lbl_dolg_value = Label(window, text="value", font=("Arial", 14))
+lbl_dolg_value['text'] = findto(8, all)
+lbl_dolg_value.grid(column=2, row=6)
+lbl_family_value = Label(window, text="value", font=("Arial", 14))
+#lbl_family_value['text'] = findto(2, all)
+lbl_family_value.grid(column=3, row=6)
+
+#seven
+lbl_interes = Label(window, text="Интерес(3)", font=("Arial Bold", 18))
+lbl_interes.grid(column=0, row=7)
+lbl_trud = Label(window, text="Труд(6)", font=("Arial Bold", 18))
+lbl_trud.grid(column=1, row=7)
+lbl_memory = Label(window, text="Память(9)", font=("Arial Bold", 18))
+lbl_memory.grid(column=2, row=7)
+lbl_privichki = Label(window, text="Привычки(8)", font=("Arial Bold", 18))
+lbl_privichki.grid(column=3, row=7)
+
+lbl_interes_value = Label(window, text="value", font=("Arial", 14))
+lbl_interes_value['text'] = findto(3, all)
+lbl_interes_value.grid(column=0, row=8)
+lbl_trud_value = Label(window, text="value", font=("Arial", 14))
+lbl_trud_value['text'] = findto(6, all)
+lbl_trud_value.grid(column=1, row=8)
+lbl_memory_value = Label(window, text="value", font=("Arial", 14))
+lbl_memory_value['text'] = findto(9, all)
+lbl_memory_value.grid(column=2, row=8)
+lbl_privichki_value = Label(window, text="value", font=("Arial", 14))
+lbl_privichki_value['text'] = findto(8, all)
+lbl_privichki_value.grid(column=3, row=8)
+
+#nine
+lbl_bit = Label(window, text="Быт(4)", font=("Arial Bold", 18))
+lbl_bit.grid(column=0, row=9)
+lbl_talant = Label(window, text="Талант(7)", font=("Arial Bold", 18))
+lbl_talant.grid(column=1, row=9)
+
+lbl_bit_value = Label(window, text="value", font=("Arial", 14))
+lbl_bit_value['text'] = findto(4, all)
+lbl_bit_value.grid(column=0, row=10)
+lbl_talant_value = Label(window, text="value", font=("Arial", 14))
+lbl_talant_value['text'] = findto(7, all)
+lbl_talant_value.grid(column=1, row=10)
+
+# must be end
+window.mainloop()
